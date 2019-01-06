9eb86d12559a8224b30eae050bd4ea4d63f96d94
loranazarenko <loranazarenko@users.noreply.github.com>
2019-01-07 00:36:21 +0200

    1

--- stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// Factorial.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
